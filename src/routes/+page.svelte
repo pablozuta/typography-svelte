30ddbf4157901f90d79644df6a06844251fa1f6a
Pablo Zuta <pablo.zuta.h@gmail.com>
2022-12-17 15:17:55 -0400

    se cambian nombres imagenes en cloudinary y agregan nuevos links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,94 +1,172 @@
 <h1>Introduction To Typography</h1>
 
-<hr>
+<hr />
 <h3>What is Type?</h3>
-<p>Well let's start by sorting out two terms that get confused a lot, font and typeface.
-This might seem like a nit picky distinction, but it's a distinction that actually reflects the dual nature of type.</p>
-<p>On the one hand, type is a tangible thing, a movable kit of pre-designed forms that can be shuffled around to make words. And on the other hand, it's something less tangible, a work of design that can be represented and re-represented at many sizes and in many formats.</p>
+<p>
+	Well let's start by sorting out two terms that get confused a lot, font and typeface. This might
+	seem like a nit picky distinction, but it's a distinction that actually reflects the dual nature
+	of type.
+</p>
+<p>
+	On the one hand, type is a tangible thing, a movable kit of pre-designed forms that can be
+	shuffled around to make words. And on the other hand, it's something less tangible, a work of
+	design that can be represented and re-represented at many sizes and in many formats.
+</p>
 
-<img src="https://res.cloudinary.com/ddkgbgwvp/image/upload/c_scale,h_593,w_745/v1668576809/Typography%20Svelte/Screenshot_20221112-121315_Coursera_r9hnyq.jpg"
-	alt="california job case">
-<p>A font originally referred to one set of metal type.In a California job case, a standard filing system for metal
-	typet hat has the letters and other typographic elements arranged according to frequency of use, not unlike the way
-	that the keys are arranged on a computer keyboard. Metal type obviously can't be made bigger or smaller or italic or
-	bold. So a font of metal type is just one type design in one style, in one weight at one size.
-	Movable type like this was first invented in China around 1040 by an inventor named Bi Sheng. It was reinvented 400
-	years later in Germany in the 15th century by Johannes Gutenberg. Gutenberg carved his letter forms at actual scale
-	and then cast them in a mixture of metals, mostly lead.</p>
-<img src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668576808/Typography%20Svelte/Screenshot_20221112-121324_Coursera_v3t991.jpg"
-	alt="bi sheng">
 
-<img src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668576810/Typography%20Svelte/Screenshot_20221112-121329_Coursera_ymms1c.jpg"
-	alt="johannes gutenberg">
-<p>The process of letterpress printing remained the main technology for setting and reproducing type from its invention
-	in the 1450's up into the 1960's. And even today, a lot of the vocabulary we use to talk about type references
-	letterpress technology. In the 1960s and 70s, metal typesetting was replaced by photo typesetting. A photo type
-	font,was essentially a slide or film reel of letter forms that was projected in sequence onto photo paper.
 
-	Today, a font of type is a .otf or .ttf file stored on your computer. These files contain vector information
-	describing the shapes of the letter forms, and also information about the space between different combinations of
-	letters.
-	So a font is the physical manifestation of type, whether in metal, film or code.</p>
 
-<img src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668576809/Typography%20Svelte/Screenshot_20221112-121349_Coursera_lasxay.jpg"
-	alt="">
-<img src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668577796/Typography%20Svelte/Screenshot_20221112-122125_Coursera_bauzfi.jpg" alt="photo typesetting">
-<p>In contrast to a font, typeface describes the visual design of a particular set of letter forms.
-	Typefaces are massive design projects. To create a fully functioning contemporary typeface, a designer must create
-	hundreds and thousands of carefully considered individual drawings.</p>
-<p>At minimum, typefaces these days include uppercase letters, lowercase letters, numbers called figures, often in
-	several different styles,fractions and mathematical symbols, punctuation and special characters, accented characters
-	in both upper and lower case.
-	And ligatures, specially drawn combinations of two or more letters.
-	All of these characters must be individually drawn in both regular and italic styles. And both of these styles must
-	be drawn in at least three weights. Different degrees of boldness. So type faces are time consuming and skill
-	demanding works of design.
-	And like other works of design, they're protected as intellectual property. In order to use a type face in the piece
-	of design work, you need to have a license to do so.
-	The typefaces that come with our operating system or other software have been licensed for us by the software
-	company. But most other high quality typefaces must be purchased and downloaded from their designer or distributor
-	in order to be used legally.</p>
+<p>
+	A font originally referred to one set of metal type.In a California job case, a standard filing
+	system for metal typet hat has the letters and other typographic elements arranged according to
+	frequency of use, not unlike the way that the keys are arranged on a computer keyboard. Metal type
+	obviously can't be made bigger or smaller or italic or bold. So a font of metal type is just one
+	type design in one style, in one weight at one size. Movable type like this was first invented in
+	China around 1040 by an inventor named Bi Sheng. It was reinvented 400 years later in Germany in
+	the 15th century by Johannes Gutenberg. Gutenberg carved his letter forms at actual scale and then
+	cast them in a mixture of metals, mostly lead.
+</p>
+
+<div class="grid-2">
+       
+	<div class="card">
+	 <img src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668576809/Typography%20Svelte/california-box.jpg" class="big-image" alt="california box">
+	 </div>
+		
+</div><!--fin grid-->
+<div class="grid-2">
+       
+	<div class="card">
+	 <img src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668576810/Typography%20Svelte/gutenberg.jpg" class="artist__img" alt="gutenberg">
+	 </div>
+		   
+			
+	<div class="card">
+	 <img src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668576808/Typography%20Svelte/bi-sheng.jpg" class="artist__img" alt="bi sheng" >
+	  </div>
+
+</div><!--fin grid-->
+
+<p>
+	The process of letterpress printing remained the main technology for setting and reproducing type
+	from its invention in the 1450's up into the 1960's. And even today, a lot of the vocabulary we
+	use to talk about type references letterpress technology. In the 1960s and 70s, metal typesetting
+	was replaced by photo typesetting. A photo type font,was essentially a slide or film reel of
+	letter forms that was projected in sequence onto photo paper. Today, a font of type is a .otf or
+	.ttf file stored on your computer. These files contain vector information describing the shapes of
+	the letter forms, and also information about the space between different combinations of letters.
+	So a font is the physical manifestation of type, whether in metal, film or code.
+</p>
+
+<img
+	src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668576809/Typography%20Svelte/Screenshot_20221112-121349_Coursera_lasxay.jpg"
+	alt=""
+/>
+<img
+	src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668577796/Typography%20Svelte/Screenshot_20221112-122125_Coursera_bauzfi.jpg"
+	alt="photo typesetting"
+/>
+<p>
+	In contrast to a font, typeface describes the visual design of a particular set of letter forms.
+	Typefaces are massive design projects. To create a fully functioning contemporary typeface, a
+	designer must create hundreds and thousands of carefully considered individual drawings.
+</p>
+<p>
+	At minimum, typefaces these days include uppercase letters, lowercase letters, numbers called
+	figures, often in several different styles,fractions and mathematical symbols, punctuation and
+	special characters, accented characters in both upper and lower case. And ligatures, specially
+	drawn combinations of two or more letters. All of these characters must be individually drawn in
+	both regular and italic styles. And both of these styles must be drawn in at least three weights.
+	Different degrees of boldness. So type faces are time consuming and skill demanding works of
+	design. And like other works of design, they're protected as intellectual property. In order to
+	use a type face in the piece of design work, you need to have a license to do so. The typefaces
+	that come with our operating system or other software have been licensed for us by the software
+	company. But most other high quality typefaces must be purchased and downloaded from their
+	designer or distributor in order to be used legally.
+</p>
 
 <h3>Stroke and Proportion</h3>
-<p>What really define the character , the look and feel of a typeface on actually use is not it's details but some
-	overall qualities of stroke & proportion , and these qualities are what typographers pays most attention to when
-	they're are looking at types.
-	The forms of the typographic alphabet emerged from scribes' handwriting in the 15th century. So the lines that form
-	letters are called strokes and are based on calligraphic marks.</p>
-	<img src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668577795/Typography%20Svelte/Screenshot_20221112-122402_Coursera_tstr5j.jpg" alt="">
-<p>Weight is the thickness of a stroke, and this varies between typefaces and also within type families.
-	Stroke weight contributes to the type's color, how dark or light it appears when set in text.
-	Stroke modulation is how much the thickness of the stroke changes in different parts of the letter.</p>
-	<img src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668578311/Typography%20Svelte/Screenshot_20221112-122501_Coursera_d1jpk9.jpg" alt="">
-	
-<p>In traditional calligraphy, the brusher pen is held at a constant angle as the letters are drawn, creating a stroke
-	with a lot of modulation.
-	The modulation of a stroke implies a tilt or axis to the letterform. In more calligraphic typefaces, this axis is
-	usually angled and in more mechanical typefaces it tends to be vertical.</p>
-<p>In humanistic typefaces, strokes often end with a little wedge-shaped form called a seraph The presence or absence of
-	serifs divide all typefaces into the categories of serif and sans serif. Sans from the French, meaning without. And
-	these two styles have quite different textures on the page.
-	Some designers argue that serifs make type easier to read by creating an imaginary line that the type sits on, and
-	that guides our eyes along each line of text. And for this reason, serif type faces are conventionally seen as more
-	appropriate for long bodies of text.</p>
-<p>Besides qualities of stroke, the overall appearance of a typeface is also determined by its proportions.
-	Some typefaces have narrow and tall condensed letter forms, while others have squat and wide extended forms.
-	That small scale condensed type on the left looks blocky and dense, extended type looks kind of airy.</p>
-<p>Another important proportion is what might roughly be described as the size relationship between lowercase and
-	uppercase letters. And this is called the x-height of the typeface. The x-height is the imaginary line that defines
-	the height of many of the lowercase letters.
-	Typefaces with a large x-height with a proportionally larger lowercase letters, like this typeface, look bigger and
-	more even on the page. Typefaces with a small x-height with proportionally smaller lowercase letters look smaller
-	and more delicate.</p>
-<p>Taken all together, these over all qualities of proportion and of stroke are what really give a typeface it's
-	particular appearance at type size.</p>
+<p>
+	What really define the character , the look and feel of a typeface on actually use is not it's
+	details but some overall qualities of stroke & proportion , and these qualities are what
+	typographers pays most attention to when they're are looking at types. The forms of the
+	typographic alphabet emerged from scribes' handwriting in the 15th century. So the lines that form
+	letters are called strokes and are based on calligraphic marks.
+</p>
+<img
+	src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668577795/Typography%20Svelte/Screenshot_20221112-122402_Coursera_tstr5j.jpg"
+	alt=""
+/>
+<p>
+	Weight is the thickness of a stroke, and this varies between typefaces and also within type
+	families. Stroke weight contributes to the type's color, how dark or light it appears when set in
+	text. Stroke modulation is how much the thickness of the stroke changes in different parts of the
+	letter.
+</p>
+<img
+	src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1668578311/Typography%20Svelte/Screenshot_20221112-122501_Coursera_d1jpk9.jpg"
+	alt=""
+/>
 
-
-
+<p>
+	In traditional calligraphy, the brusher pen is held at a constant angle as the letters are drawn,
+	creating a stroke with a lot of modulation. The modulation of a stroke implies a tilt or axis to
+	the letterform. In more calligraphic typefaces, this axis is usually angled and in more mechanical
+	typefaces it tends to be vertical.
+</p>
+<p>
+	In humanistic typefaces, strokes often end with a little wedge-shaped form called a seraph The
+	presence or absence of serifs divide all typefaces into the categories of serif and sans serif.
+	Sans from the French, meaning without. And these two styles have quite different textures on the
+	page. Some designers argue that serifs make type easier to read by creating an imaginary line that
+	the type sits on, and that guides our eyes along each line of text. And for this reason, serif
+	type faces are conventionally seen as more appropriate for long bodies of text.
+</p>
+<p>
+	Besides qualities of stroke, the overall appearance of a typeface is also determined by its
+	proportions. Some typefaces have narrow and tall condensed letter forms, while others have squat
+	and wide extended forms. That small scale condensed type on the left looks blocky and dense,
+	extended type looks kind of airy.
+</p>
+<p>
+	Another important proportion is what might roughly be described as the size relationship between
+	lowercase and uppercase letters. And this is called the x-height of the typeface. The x-height is
+	the imaginary line that defines the height of many of the lowercase letters. Typefaces with a
+	large x-height with a proportionally larger lowercase letters, like this typeface, look bigger and
+	more even on the page. Typefaces with a small x-height with proportionally smaller lowercase
+	letters look smaller and more delicate.
+</p>
+<p>
+	Taken all together, these over all qualities of proportion and of stroke are what really give a
+	typeface it's particular appearance at type size.
+</p>
 
 <!--aca comienzan los estilos de la pagina-->
 <style>
 	hr {
 		border: 1px solid black;
+	}
+
+	.grid-2{
+    margin-top: 0.1rem;
+	display: grid;
+	grid-template-columns:repeat(auto-fit, minmax(21rem, 1fr)) ;
+	padding: 2%;
+	gap: 3rem;
+	}
+	
+	.artist__img{
+		width: 100%;
+		aspect-ratio: 1/1;
+		object-fit: cover;
+	}
+	.big-image{
+		width: 100%;
+		aspect-ratio: 1/0.5;
+		object-fit: cover;
+	}@media(max-width:800px){
+		.big-image{
+			aspect-ratio: 1/0.8;
+		}
 	}
 </style>

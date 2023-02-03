7c2dc698b414a1b1f0c1b27c62c8087c23c01d2c
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-02-03 14:53:23 -0400

    se termina de dar formato a paginas

--- src/routes/page-space/+page.svelte
+++ src/routes/page-space/+page.svelte
@@ -1,29 +1,83 @@
-[Page Space]
-
-In the last video, we looked at the spaces within a piece of set type. In this video, we're going to zoom out a bit and look at the spaces between and around pieces of set type within a composition.
-A piece of typography is both form, a visual element with shape, texture, figure and ground, and direction, and language, an expression of thought with grammar, syntax, and rhythm.
-As typographers arrange a type and space, we have to consider both the way that space effects composition, and the way that it effects meaning. We have to train our eyes both to see space and to read space.
-Let's start with the reading part.
-The spatial principle that most directly affects the meaning of text is the principle of proximity. We tend to read things that are closer together as related and things that are further apart as less related.
-Here it's unclear how we're supposed to read the second line of text, 1850-1900, because it's positioned ambiguously halfway between the title and the text. If we nudge it up a bit we now read it as related to the title, as a subtitle. So we read that this article is about typography between the years 1850 and 1900. If we move the same line down, it now reads as related to the paragraph below it as a section header. So now we read that this article is about typography in general, and that this first section in the article is about the years 1850 to 1900. When you're positioning type like this, it's a good idea to use distances that relate to your leading.
-For example, you might make this distance two times the leading, and this distance five times the leading.
-It's really difficult to overstate the importance of this kind of close attention to spacing. It's often the difference between clear typography and confusing typography.
-This timeline of typefaces, for instance, is really confusing because it's not clear what information is related to what.
-By changing the vertical spacing, we can fix a lot of the problem.
-Playing with the horizontal spacing, we can further improve it. And now it's clear, for instance, that both Helvetica and Univers were designed in the year 1957. We also use space, either horizontal or vertical, to signal syntactical divisions in text, like paragraph breaks. And the traditional way to do this is with an indent.
-The depth of an indent is often set to one the size of the type. Or to the size of the leading.
-Indents are used to mark the division between two paragraphs of text. So the first paragraph of a body of text with nothing in front of it should not be indented.
-You can also signal paragraph breaks with a white line, a vertical space the size of the letting. But you should never use both a white line and an indent because both mean the same thing.
-Zooming out the same principles of proximity apply to the arrangement of typographical elements in a composition, such as a page spread.
-Here the small paragraph on the left reads as related to the larger one as a note perhaps about something that's mentioned in the larger paragraph.
-Here positioned further away it reads as less related.
-In this arrangement the paragraphs of text read as a single connected narrative because they're flowed one after the next.
-Whereas here they look like there maybe for entries on four different topics.
-In these examples, I'm representing type as a series of lines. The old term for this is greeking. It's used in sketching out layouts but it also helps us switch modes from reading the type to seeing it as abstract form. In this mode, we can apply some of the fundamental principles of formal composition to our layout. We can create contrast. For instance, we can make some type small, light, and tracked out and other type large, bold, and tightly tracked.
-We can consider compositional direction. This composition, for instance, has a horizontal emphasis. Whereas this one feels more vertical.
-And finally, we can consider the relationship between positive space and negative space.
-Here, the negative space is just a frame surrounding the typography, which feels kind of static.
-But here, I've moved things around so that the negative space is a more interesting, more energetic shape.
-As you can see, it's not easy to create a typographic composition that both reads well and looks good. But it's this challenge that makes composing type really rewarding and interesting.
-
-<button class="boton-link"><a href="/the-grid">The Grid >></a></button>
+<main>
+	<h3>Page Space</h3>
+	<p>
+		A piece of typography is both form, a visual element with shape, texture, figure and ground, and
+		direction, and language, an expression of thought with grammar, syntax, and rhythm.
+	</p>
+	<p>
+		As typographers arrange a type and space, we have to consider both the way that space effects
+		composition, and the way that it effects meaning. We have to train our eyes both to see space
+		and to read space.
+	</p>
+	<p>Let's start with the reading part.</p>
+	<p>
+		The spatial principle that most directly affects the meaning of text is the principle of
+		proximity. We tend to read things that are closer together as related and things that are
+		further apart as less related. Here it's unclear how we're supposed to read the second line of
+		text, 1850-1900, because it's positioned ambiguously halfway between the title and the text. If
+		we nudge it up a bit we now read it as related to the title, as a subtitle. So we read that this
+		article is about typography between the years 1850 and 1900. If we move the same line down, it
+		now reads as related to the paragraph below it as a section header. So now we read that this
+		article is about typography in general, and that this first section in the article is about the
+		years 1850 to 1900. When you're positioning type like this, it's a good idea to use distances
+		that relate to your leading.
+	</p>
+	<p>
+		For example, you might make this distance two times the leading, and this distance five times
+		the leading.
+	</p>
+	<p>
+		It's really difficult to overstate the importance of this kind of close attention to spacing.
+		It's often the difference between clear typography and confusing typography. This timeline of
+		typefaces, for instance, is really confusing because it's not clear what information is related
+		to what.
+	</p>
+	<p>
+		By changing the vertical spacing, we can fix a lot of the problem. Playing with the horizontal
+		spacing, we can further improve it. And now it's clear, for instance, that both Helvetica and
+		Univers were designed in the year 1957. We also use space, either horizontal or vertical, to
+		signal syntactical divisions in text, like paragraph breaks. And the traditional way to do this
+		is with an indent. The depth of an indent is often set to one the size of the type. Or to the
+		size of the leading.
+	</p>
+	<p>
+		Indents are used to mark the division between two paragraphs of text. So the first paragraph of
+		a body of text with nothing in front of it should not be indented. You can also signal paragraph
+		breaks with a white line, a vertical space the size of the letting. But you should never use
+		both a white line and an indent because both mean the same thing.
+	</p>
+	<p>
+		Zooming out the same principles of proximity apply to the arrangement of typographical elements
+		in a composition, such as a page spread.
+	</p>
+	<p>
+		Here the small paragraph on the left reads as related to the larger one as a note perhaps about
+		something that's mentioned in the larger paragraph.
+	</p>
+	<p>Here positioned further away it reads as less related.</p>
+	<p>
+		In this arrangement the paragraphs of text read as a single connected narrative because they're
+		flowed one after the next.
+	</p>
+	<p>
+		Whereas here they look like there maybe for entries on four different topics. In these examples,
+		I'm representing type as a series of lines. The old term for this is greeking. It's used in
+		sketching out layouts but it also helps us switch modes from reading the type to seeing it as
+		abstract form. In this mode, we can apply some of the fundamental principles of formal
+		composition to our layout. We can create contrast. For instance, we can make some type small,
+		light, and tracked out and other type large, bold, and tightly tracked.
+	</p>
+	<p>
+		We can consider compositional direction. This composition, for instance, has a horizontal
+		emphasis. Whereas this one feels more vertical. And finally, we can consider the relationship
+		between positive space and negative space. Here, the negative space is just a frame surrounding
+		the typography, which feels kind of static.
+	</p>
+	<p>
+		But here, I've moved things around so that the negative space is a more interesting, more
+		energetic shape. As you can see, it's not easy to create a typographic composition that both
+		reads well and looks good. But it's this challenge that makes composing type really rewarding
+		and interesting.
+	</p>
+	<button class="boton-link"><a href="/the-grid">The Grid >></a></button>
+</main>

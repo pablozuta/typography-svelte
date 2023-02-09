61deb3ed6b436b375d9ea87ece0ca87e9a2c54f4
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-02-09 19:48:07 -0400

    se agregan imagenes a 'typesetting with expression' page

--- src/routes/typesetting-with-expression/+page.svelte
+++ src/routes/typesetting-with-expression/+page.svelte
@@ -1,12 +1,8 @@
 <main>
 	<h3 class="under">Typesetting with Expression</h3>
+	
 	<p>
-		Over the last two weeks we've seen how type face choice, composition, and typesetting not only
-		determine the aesthetics of a piece of typography but also shape its meaning in subtle ways.
-	</p>
-	<p>
-		In this video, we're going to go a step further and look at how the tools of typesetting can be
-		used to dramatically express meaning through type. In traditional typography, the objective has
+		 In traditional typography, the objective has
 		usually been to present the text in as elegant, legible, and efficient a way as possible.
 		Typographic styles have historically reflected the conventions of the time period more than they
 		have the content of the text.
@@ -17,78 +13,87 @@
 	</p>
 	<p>
 		In 1916 for instance, the French surrealist poet published this concrete poem. The poem is about
-		rain and its typographic form expresses this as much as its content does. This book from around
+		rain and its typographic form expresses this as much as its content does.</p>
+		<img src="https://bonjourparis.com/wp-content/uploads/2020/06/IlPleut.png" alt="il pleut poem">
+		<p>
+		This book from around
 		the same time by the Italian futurist Felipo Marinetti sets out to capture a visceral sense of
-		war through dramatic typographic compositions. And in this book, a mid century typographic
+		war through dramatic typographic compositions.</p>
+		<img src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1675981393/Typography%20Svelte/20230207_153722_ljbsdy.jpg" alt="italian futurist book cover">
+
+		<p>
+		 And in this book, a mid century typographic
 		interpretation of a play by Eugene Ionesco, typographic compositions express the tone and pace
 		of the dialogue progressing from subdued to frenetic.
 	</p>
+	<img src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1675981392/Typography%20Svelte/20230207_153744_a53gio.jpg" alt="eugene ionesco typographic poster">
 	<p>
 		While expressive typography was sometimes at odds with the functionalist outlook of mid 20th
 		century graphic design, it made a comeback in the 1980s and 90s. This is a poster for a theater
-		company by the American designer Paula Share. Today the consensus amongst designers is that
-		there's a time and place for typography like this.
+		company by the American designer Paula Sher. 
 	</p>
-	<p>
+	<img src="https://www.creativeboom.com/uploads/articles/00/0080645fa3f8a055235920cb71f1a058491a44fa_1620.jpg" alt="paula sher typographic poster">
+	 <p>
 		It might be appropriate for a poster or a book cover but it's probably not appropriate for a
 		science text book. There are infinite ways to shape the meaning of text through typographic
 		treatment. At the most basic level, we can manipulate meaning by using the variables of
 		typeface, size, style, color, and arrangement. The same variables we use to create hierarchy.
-	</p>
-	<p>
-		By way of example, I'm going to style the title of this film, an action movie about fast cars.
-	</p>
-	<p>
-		I'll start by changing the type phase to something squarer and more industrial, something that
-		connotes machines.
-	</p>
-	<p>Then I'll change the weight to something super bold so that it feels tough and aggressive.</p>
-	<p>
-		I'll change the type to all caps to look even more aggressive. And I'll make it italic to look
-		like it's leaning forward, to connote speed. To convey more of a sense of horizontal movement,
-		I'll break the title into two lines and offset them horizontally.
-	</p>
-	<p>
-		This looks pretty good, but maybe it feels too conventional. So I'll try to differentiate the
-		two words in the title. I'll make fast smaller and lighter and track it out so it feels even
-		more nimble and speedy.
-	</p>
-	<p>
-		Now furious looks too prominent, so I'll try to restore some balance by making it a little
-		lighter. And I'll make the ampersand smaller so it gets less emphasis. Of course, if I were
-		setting the title to a different film, or TV show in this case, I'd be trying to convey a
-		different set of meanings.
-	</p>
-	<p>
-		This show is about a traditional wealthy family in Britain at the start of the 20th century. So
-		I'll choose a traditional British type face, Baskerville. I'll set it in all caps in this case
-		because that looks more formal. And I'll choose the center line to stacked arrangement because
-		that's a traditional way to set title type.
-	</p>
-	<p>
-		I'll tweak it a little to create more visual interest. I'll make Downton slightly bolder and
-		larger and Abbey slightly smaller and lighter. In reality, there are few subjects that clearly
-		suggest a particular type treatment like these do. In most cases as the designer, you'll need to
-		make a decision about what ideas or concepts you want to pull from the content and express
-		through the type treatment, and this is really a form of authorship.
-	</p>
-	<p>
-		Zooming out from a single title to a full composition, we can revisit the idea of compositional
-		gesture from last week but now with an eye toward making the gesture expressive.
-	</p>
+	 </p>
+	
 	<p>
 		We can create a composition that suggests horizontal movement, like in this poster about canal
 		boats, conjuring up the slow movement of the boats along the water. Or a composition that feels
 		vertical, like in this poster for a conference in Shanghai, a city know for its skyscrapers.
 	</p>
+	<div class="grid-2">
+		<div class="card">
+			<img
+				src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1675981392/Typography%20Svelte/20230207_153806_jzoj7n.jpg"
+				class="artist__img"
+				alt="blue poster typographic"
+			/>
+		</div>
+	
+		<div class="card">
+			<img
+				src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1675981392/Typography%20Svelte/20230207_153826_exjsgf.jpg"
+				class="artist__img"
+				alt="skycraper typographic design"
+			/>
+		</div>
+	</div>
+	<!--fin grid-->
 	<p>
 		We can make a composition that's a vortex. Here in this poster from the 1920s the vortex gesture
 		expresses the dizzy feeling of being in the bustle of a modern city. Or a composition that is
 		random and scattered, perhaps suggesting chaos or something organic, like these flies.
 	</p>
+	<div class="grid-2">
+		<div class="card">
+			<img
+				src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1675981393/Typography%20Svelte/20230207_153848_oqynk7.jpg"
+				class="artist__img"
+				alt="vortex poster typographic"
+			/>
+		</div>
+	
+		<div class="card">
+			<img
+				src="https://res.cloudinary.com/ddkgbgwvp/image/upload/v1675982537/Typography%20Svelte/Screenshot_20230207-151604_Coursera_pajwqa.jpg"
+				class="artist__img"
+				alt="flies and letters typographic design"
+			/>
+		</div>
+	</div>
+	<!--fin grid-->
+
 	<p>
 		Or in the opposite extreme, we can create a gridded and orderly composition like Armin Hofmann,
-		the mid century Swiss designer, has done in this poster about formal principles. There's no
+		the mid century Swiss designer, has done in this poster about formal principles. 
+		</p>
+		<img src="https://i0.wp.com/www.printmag.com/wp-content/uploads/2020/06/da4aaf_103b5258f84d4f6e9157f7e7eda035e2mv2.jpeg?resize=763%2C1062&ssl=1" alt="armin hofmann poster black background big letters">
+		<p>
+		There's no
 		formula here for exactly what compositional gesture or what typographic style will best express
 		a particular meaning. But that doesn't mean that designers are off the hook. Typography is
 		always about communication and a piece of expressive type is only successful if it is able to

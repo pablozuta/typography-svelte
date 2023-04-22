805c3c850c3fe21c8f19adf09e10e02188645281
pablozuta <pablo.zuta.h@gmail.com>
2023-04-22 04:10:49 -0400

    se modifica pagina 'typographic space'

--- src/routes/typographic-space/+page.svelte
+++ src/routes/typographic-space/+page.svelte
@@ -13,15 +13,8 @@
 	</p>
 	<p>
 		Adding or subtracting space between the letters in a word, sentence, or paragraph in typesetting
-		is called tracking. If you're setting type at small sizes, like nine points or below, adding a
-		little bit of tracking, say 10 or 20 units, can help make the letters more clearly legible.
-	</p>
-	<p>
-		But at large sizes, too much tracking will make a word start to visually disintegrate, because
-		the letters aren't close enough to one another to stick together as a word. In fact, if you're
-		setting a large title, you may want to set negative tracking, to bring the letters even closer
-		together than their native letter spacing.
-	</p>
+		is called tracking.</p>
+
 	<p>
 		One place for extra tracking is always appropriate is in setting all uppercase type. Because
 		uppercase letters usually appear next to lower case letters. They're designed with the tighter
@@ -33,9 +26,7 @@
 		called kerning.
 	</p>
 	<p>
-		Kerning is sometimes necessary when you have two letters that fit together awkwardly. In this
-		word, because of the shapes of the uppercase A and the v, there's some distracting extra space
-		between these two letters. So we'll kern them to be closer together. Typefaces these days are
+		Kerning is sometimes necessary when you have two letters that fit together awkwardly. Typefaces these days are
 		preprogrammed with kerning for problematic letter combinations. So manually kerning is rarely
 		necessary. The space between lines of type in a paragraph. The space between one baseline and
 		the next is called leading. The name comes from the strips of lead that used to be placed
@@ -61,28 +52,7 @@
 		66 characters per line is often seen as a sweet spot for line length, but up to 80 characters
 		still works, and down to 40 for narrower columns of text also works.
 	</p>
-	<p>
-		So how does line length affect leading? Well, if your lines are longer, you may need more space
-		between them in order to help the reader's eyes follow along them. A final variable is text
-		alignment. When text is aligned on the left side and not on the right, it's referred to as left
-		aligned.
-	</p>
-	<p>
-		The advantage of setting type this way is that it allows the typeface's native letter spacing
-		and word spacing to be preserved unaltered. The disadvantage is that it creates an uneven edge
-		along one side of the text, called a rag. And in a future video we'll look at how this rag can
-		be manipulated to make it as visually pleasing as possible.
-	</p>
-	<p>
-		An alternative to left aligned text is justified text, text that has been aligned on both the
-		left and right sides. The advantage of setting type like this is that it creates even edges on
-		both sides.
-	</p>
-	<p>
-		But the disadvantages that word spacing and/or tracking have to be altered in order to make
-		longer lines shorter and shorter lines longer. And this breaks up the lines of text, creating
-		vertical channels through the paragraph, and has to be compensated for by increasing leading.
-	</p>
+	
 	<p>
 		So as you can see the variables that define typographic space, tracking, leading, line length,
 		and alignment all effect one another. So as a typographer it's important to be really aware of
@@ -93,3 +63,9 @@
 		<a class="link-routes" href="/creating-hierarchy">Next ></a>
 	</div>
 </main>
+
+<style>
+	main {
+		background-color: bisque;
+	}
+</style>
